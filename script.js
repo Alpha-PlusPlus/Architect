338b3cc62a07c64414e72585b6cc54ebe6ad5795
Jon Alpha <Death77485@aol.com>
2022-03-18 22:59:31 -0500

    Added and styled section 4 (contact) and footer

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
     document.querySelector('.container').classList.toggle('change');
 });
+
+// Scroll Btn
+
+// document.querySelector('.scroll-btn').addEventListener('click', () => {
+
+//     document.querySelector('html').style.scrollBehavior = "smooth";
+
+//     setTimeout(() => {
+//         document.querySelector('html').style.scrollBehavior = "unset";
+//     }, 1000)
+
+// });
